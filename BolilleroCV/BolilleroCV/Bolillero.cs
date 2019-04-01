a849c57073e6a9f8f677cf45747ac27c0dff7d07
VeronicaChoque <jinxsona69@gmail.com>
2019-04-01 17:09:07 -0300

    010419

---
+++ BolilleroCV/BolilleroCV/Bolillero.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BolilleroCV
+{
+    public class Bolillero
+    {
+        Random r;
+        public List<int> bolillas { get; set; }
+        public Bolillero()
+        {
+            var bolillas = new List<byte>();
+            var r = new Random();
+        }
+
+        public byte sacarBolilla()
+        {
+            bolillas = r.Next(0,);
+
+        }
+        public void volverAponer()
+        {
+
+        }
+    }
+}
