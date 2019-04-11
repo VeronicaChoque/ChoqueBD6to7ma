74f24757a440e4febaec264923f5e4881c620786
VeronicaChoque <jinxsona69@gmail.com>
2019-04-11 15:40:48 -0300

    simularsinhilos

--- BolilleroCV/BolilleroCV/Simulacion.cs
+++ BolilleroCV/BolilleroCV/Simulacion.cs
@@ -6,27 +6,27 @@ using System.Threading.Tasks;
 
 namespace BolilleroCV
 {
-    class Simulacion
+    public class Simulacion
     {
         public Bolillero Bolillero { get; set; }
         public long CantidadSimulaciones { get; set; }
         public long CantidadAciertos { get; private set; }
 
-        //public double PorcentajeAciertos
-        //{
-        //    get
-        //    {
-        //        return CantidadAciertos * 100 / CantidadSimulaciones;
-        //    }
-        //}
+        public double PorcentajeAciertos
+        {
+            get
+            {
+                return CantidadAciertos * 100 / CantidadSimulaciones;
+            }
+        }
 
         public long simularSinHilos(List<int> jugada,long CantidadSimulaciones)
         {
             return (long)Bolillero.jugar(jugada, CantidadSimulaciones); 
         }
 
-        
-        public void simularConHilos(int cantidadHilos,long CantidadSimulaciones, List<int>jugada)
+
+        public void simularConHilos(int cantidadHilos, long CantidadSimulaciones, List<int> jugada)
         {
             List<Task<long>> hilos = new List<Task<long>>();
             //List<Bolillero> bolillero = new List<Bolillero>();
@@ -39,29 +39,12 @@ namespace BolilleroCV
                 Task<long> tarea = new Task<long>(() => bolilleroClon.jugar(jugada, CantidadSimulaciones));
                 hilos.Add(tarea);
             }
-            
-            hilos.ForEach(hilo => hilo.Start());      
-            while (!hilos.TrueForAll(hilo => hilo.IsCompleted));
-            
-            this.CantidadAciertos = 0;            
+
+            hilos.ForEach(hilo => hilo.Start());
+            while (!hilos.TrueForAll(hilo => hilo.IsCompleted)) ;
+
+            this.CantidadAciertos = 0;
             hilos.ForEach(hilo => CantidadAciertos += hilo.Result);
-        }
-
-
-        //public long simularCon(Bolillero bolillero0,int jugada,int cantSimu)
-        //{
-        //    long aciertos = 0;
-        //    for (ulong i = 0; i < cantSimu; i++)
-        //    {
-        //        bolillero0.sacarBolilla();
-        //        if (bolillero0.jugar()==true)
-        //        {         
-        //            aciertos++;
-        //        }
-        //    }
-        //    return aciertos;}
-        
-
-       
+        }      
     }
 }
---
+++ BolilleroCV/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using BolilleroCV;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        Bolillero bolillero;
+        Simulacion simulacion;
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            bolillero = new Bolillero();
+            bolillero.bolillasAdentro = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            simulacion = new Simulacion();
+
+
+        }
+    }
+}
